d1e38032cbff79e1e16c559b6f36306d50d6fb31
baguchan <shutux500@gmail.com>
2024-03-28 17:17:40 +0900

    just a slingshot

--- forge/src/main/java/com/github/teamfusion/just_a_slingshot/forge/JustASlingShotForge.java
+++ forge/src/main/java/com/github/teamfusion/just_a_slingshot/forge/JustASlingShotForge.java
@@ -1,13 +1,11 @@
 package com.github.teamfusion.just_a_slingshot.forge;
 
 import com.github.teamfusion.just_a_slingshot.JustASlingShot;
-import com.github.teamfusion.just_a_slingshot.platform.common.worldgen.BiomeManager;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(JustASlingShot.MOD_ID)
 public class JustASlingShotForge {
     public JustASlingShotForge() {
         JustASlingShot.bootstrap();
-        BiomeManager.setup();
     }
 }
