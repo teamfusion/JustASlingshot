de85e9feebe8e2feac19e2794a9395f0acc163e8
baguchan <shutux500@gmail.com>
2025-05-11 10:13:46 +0900

    honey drip

--- common/src/main/java/com/github/teamfusion/just_a_slingshot/mixin/LivingEntityMixin.java
+++ common/src/main/java/com/github/teamfusion/just_a_slingshot/mixin/LivingEntityMixin.java
@@ -7,15 +7,29 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.UUID;
+
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity implements IHoney {
+    @Shadow
+    @Nullable
+    public abstract AttributeInstance getAttribute(Attribute attribute);
+
+    private static final UUID SPEED_MODIFIER_HONEY_UUID = UUID.fromString("80302681-6d85-459b-884c-426d52a96beb");
+
     @Unique
     private int just_a_slingshot$honeyTick;
 
@@ -29,8 +43,16 @@ public abstract class LivingEntityMixin extends Entity implements IHoney {
             --this.just_a_slingshot$honeyTick;
             if (!this.level().isClientSide() && this.level() instanceof ServerLevel serverLevel) {
                 serverLevel.sendParticles(ParticleTypes.FALLING_HONEY, this.getRandomX(this.getBbWidth()), this.getRandomY(), this.getRandomZ(this.getBbWidth()), 1, 0D, 0D, 0D, 0D);
+
             }
         }
+
+    }
+
+    @Inject(method = "aiStep", at = @At("HEAD"))
+    public void aiTick(CallbackInfo ci) {
+        this.just_a_slingshot$removeHoney();
+        this.just_a_slingshot$tryAddHoney();
     }
 
     @Unique
@@ -51,5 +73,32 @@ public abstract class LivingEntityMixin extends Entity implements IHoney {
     @Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
     protected void readAdditionalSaveData(CompoundTag compoundTag, CallbackInfo ci) {
         this.setJust_a_slingshot$honeyTick(compoundTag.getInt("HoneyTick"));
+    }
+
+    @Unique
+    protected void just_a_slingshot$removeHoney() {
+        AttributeInstance attributeInstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
+        if (attributeInstance != null) {
+            if (attributeInstance.getModifier(SPEED_MODIFIER_HONEY_UUID) != null) {
+                attributeInstance.removeModifier(SPEED_MODIFIER_HONEY_UUID);
+            }
+
+        }
+    }
+
+    @Unique
+    protected void just_a_slingshot$tryAddHoney() {
+        int i = this.getJust_a_slingshot$honeyTick();
+        if (i > 0) {
+            AttributeInstance attributeInstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
+            if (attributeInstance == null) {
+                return;
+            }
+
+            float f = -0.05F;
+            attributeInstance.addTransientModifier(new AttributeModifier(SPEED_MODIFIER_HONEY_UUID, "Honey slow", (double) f, AttributeModifier.Operation.ADDITION));
+        }
+
+
     }
 }
